ef9fc2b4eb7aaeac40b786b8a20f6269d4c76e2f
sowjanya.gollapathi <sowjanya.gollapathi@gmail.com>
2022-01-20 18:08:12 +0530

    Add project files.

---
+++ WebApplication31/Migrations/20220117114534_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication31.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shopping",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(nullable: true),
+                    ProductPrice = table.Column<string>(nullable: true),
+                    ProductQuality = table.Column<string>(nullable: true),
+                    ProductColour = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shopping", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shopping");
+        }
+    }
+}
---
+++ WebApplication31/Models/Shopping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication31.Models
+{
+    public class Shopping
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductPrice { get; set; }
+        public string ProductQuality { get; set; }
+        public string ProductColour { get; set; }
+
+    }
+}
